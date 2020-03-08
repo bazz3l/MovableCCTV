684eea6898f9fcffc09a06fdcc6a2b6ca4a40b9e
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2020-03-08 11:17:53 +0000

    Better performance

--- MovableCCTV.cs
+++ MovableCCTV.cs
@@ -1,13 +1,15 @@
+using System.Linq;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Movable CCTV", "Bazz3l", "1.0.1")]
+    [Info("Movable CCTV", "Bazz3l", "1.0.3")]
     [Description("Movable cctv cameras using WASD")]
     class MovableCCTV : CovalencePlugin
     {
         #region Fields
         readonly float RotateSpeed = 0.2f;
+        public static MovableCCTV plugin;
         #endregion
 
         #region Config
@@ -45,7 +47,13 @@ namespace Oxide.Plugins
 
         void Init()
         {
+            plugin = this;
             config = Config.ReadObject<PluginConfig>();
+        }
+
+        void Unload()
+        {
+            foreach (var obj in UnityEngine.Object.FindObjectsOfType<CameraMover>().ToList()) GameObject.Destroy(obj);
         }
 
         void OnEntitySpawned(CCTV_RC cctv)
@@ -56,19 +64,59 @@ namespace Oxide.Plugins
             }
         }
 
-        void OnPlayerInput(BasePlayer player, InputState inputState)
+        void OnEntityMounted(ComputerStation station, BasePlayer player)
         {
-            ComputerStation station = player.GetMounted() as ComputerStation;
-            if (station == null || !station.currentlyControllingEnt.IsValid(true) || station.currentlyControllingEnt.Get(true).GetComponent<CCTV_RC>().IsStatic())
+            if (player.GetComponent<CameraMover>() == null)
             {
-                return;
+                player.gameObject.AddComponent<CameraMover>();
+            }
+        }
+
+        void OnEntityDismounted(ComputerStation station, BasePlayer player)
+        {
+            var cameraMover = player.GetComponent<CameraMover>();
+            if (cameraMover == null) return;
+            cameraMover.Destroy();
+        }
+
+        public class CameraMover : MonoBehaviour
+        {
+            public BasePlayer player { get; set; }
+            public ComputerStation station { get; set; }
+
+            public void Awake()
+            {
+                player = GetComponent<BasePlayer>();
+                station = player.GetMounted() as ComputerStation;
             }
 
-            float y = inputState.IsDown(BUTTON.FORWARD) ? 1f : (inputState.IsDown(BUTTON.BACKWARD) ? -1f : 0f);
-            float x = inputState.IsDown(BUTTON.LEFT) ? -1f : (inputState.IsDown(BUTTON.RIGHT) ? 1f : 0f);
+            private void FixedUpdate()
+            {
+                if (player == null || player.IsSleeping() || !player.IsConnected)
+                {
+                    Destroy();
+                    return;
+                }
 
-            inputState.current.mouseDelta.y = y * config.RotateSpeed;
-            inputState.current.mouseDelta.x = x * config.RotateSpeed;
+                if (station == null || !station.currentlyControllingEnt.IsValid(true) || station.currentlyControllingEnt.Get(true).GetComponent<CCTV_RC>().IsStatic())
+                {
+                    return;
+                }
+
+                float y = player.serverInput.IsDown(BUTTON.FORWARD) ? 1f : (player.serverInput.IsDown(BUTTON.BACKWARD) ? -1f : 0f);
+                float x = player.serverInput.IsDown(BUTTON.LEFT) ? -1f : (player.serverInput.IsDown(BUTTON.RIGHT) ? 1f : 0f);
+
+                InputState inputState = new InputState();
+                inputState.current.mouseDelta.y = y * plugin.config.RotateSpeed;
+                inputState.current.mouseDelta.x = x * plugin.config.RotateSpeed;
+
+                station.currentlyControllingEnt.Get(true).GetComponent<CCTV_RC>().UserInput(inputState, player);
+            }
+
+            public void Destroy()
+            {
+                Destroy(this);
+            }
         }
         #endregion
     }

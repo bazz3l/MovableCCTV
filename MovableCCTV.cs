8f7c2cc4d95ca8a28ada6788419f66e7a3fd48cf
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2021-02-08 22:46:31 +0000

    config/code cleanup
    Fixed camera message showing for drone.
    Configurable ui, color, position, size
    General code cleanup

--- MovableCCTV.cs
+++ MovableCCTV.cs
@@ -1,30 +1,27 @@
 using System.Collections.Generic;
 using System.Linq;
-using Oxide.Game.Rust.Cui;
 using Newtonsoft.Json;
+using Oxide.Game.Rust.Cui;
 using UnityEngine;
+using VLB;
 
 namespace Oxide.Plugins
 {
     [Info("Movable CCTV", "Bazz3l", "1.0.9")]
-    [Description("Allows player to control placed cameras.")]
-    public class MovableCCTV : RustPlugin
+    [Description("Allow players to control placed cctv cameras using WASD")]
+    class MovableCCTV : CovalencePlugin
     {
         #region Fields
-        
-        private const string MoveSound = "assets/prefabs/deployable/playerioents/detectors/hbhfsensor/effects/detect_up.prefab";
-        private const string PermUse = "movablecctv.use";
-        private const string PanelName = "cctv_panel";
-        
-        private CuiElementContainer _uiElements;
-        private CuiTextComponent _textLabel;
+
+        private const string PERM_USE = "movablecctv.use";
+        private static MovableCCTV _plugin;
         private PluginConfig _config;
 
         #endregion
 
         #region Config
-        
-        protected override void LoadDefaultConfig() => _config = GetDefaultConfig();
+
+        protected override void LoadDefaultConfig() => _config = new PluginConfig();
 
         protected override void LoadConfig()
         {
@@ -38,32 +35,35 @@ namespace Oxide.Plugins
                 {
                     throw new JsonException();
                 }
+                
+                if (!_config.ToDictionary().Keys.SequenceEqual(Config.ToDictionary(x => x.Key, x => x.Value).Keys))
+                {
+                    PrintWarning("Config was updated");
+                    
+                    SaveConfig();
+                }
             }
             catch
             {
-                LoadDefaultConfig();
-
-                SaveConfig();
+                PrintWarning("Invalid config, default config has been loaded.");
                 
-                PrintError("Loaded default configuration.");
+                LoadDefaultConfig();
             }
         }
 
         protected override void SaveConfig() => Config.WriteObject(_config, true);
 
-        private PluginConfig GetDefaultConfig()
-        {
-            return new PluginConfig
-            {
-                RotateSound = true,
-                RotateSpeed = 0.2f
-            };
-        }
-
         private class PluginConfig
         {
-            public float RotateSpeed;
-            public bool RotateSound;
+            public float RotateSpeed = 0.2f;
+            public string TextColor = "1 1 1 0.5";
+            public int TextSize = 14;
+            public string AnchorMin = "0.293 0.903";
+            public string AnchorMax = "0.684 0.951";
+
+            public string ToJson() => JsonConvert.SerializeObject(this);
+            
+            public Dictionary<string, object> ToDictionary() => JsonConvert.DeserializeObject<Dictionary<string, object>>(ToJson());
         }
         
         #endregion
@@ -72,165 +72,231 @@ namespace Oxide.Plugins
         
         protected override void LoadDefaultMessages()
         {
-            lang.RegisterMessages(new Dictionary<string, string> { {"mounted", "Control cameras using W A S D."} }, this);
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "Description", "Control the camera using WASD" }
+            }, this);
         }
         
         #endregion
 
         #region Oxide
-        
+
         private void OnServerInitialized()
         {
-            foreach (CCTV_RC cctv in BaseNetworkable.serverEntities.OfType<CCTV_RC>())
-            {
-                if (cctv.IsStatic())
-                {
-                    continue;
-                }
-
-                cctv.hasPTZ = true;
-            }
-
-            CreateUI();
+            permission.RegisterPermission(PERM_USE, this);
+            
+            CheckCCTV();
         }
 
-        private void Init() => permission.RegisterPermission(PermUse, this);
+        private void Init()
+        {
+            _plugin = this;
+
+            LoadConfig();
+        }
 
         private void Unload()
         {
-            foreach (BasePlayer player in BasePlayer.activePlayerList)
+            foreach (CameraMover cameraMover in UnityEngine.Object.FindObjectsOfType<CameraMover>())
             {
-                DestroyUI(player);
+                cameraMover.Destroy();
             }
+
+            UI.RemoveAll();
+
+            _plugin = null;
         }
 
-        private void OnEntitySpawned(CCTV_RC cctv)
+        private void OnEntityBuilt(Planner plan, GameObject go)
         {
-            if (cctv.IsStatic())
-            {
-                return;
-            }
+            CCTV_RC cctvRc = go.ToBaseEntity() as CCTV_RC;
 
-            cctv.hasPTZ = true;
-        }
-
-        private void OnEntityMounted(ComputerStation station, BasePlayer player)
-        {
-            if (!permission.UserHasPermission(player.UserIDString, PermUse))
-            {
-                return;
-            }
-
-            UpdateUI(player);
-        }
-
-        private void OnEntityDismounted(ComputerStation station, BasePlayer player)
-        {
-            if (!permission.UserHasPermission(player.UserIDString, PermUse))
-            {
-                return;
-            }
-
-            DestroyUI(player);
-        }
-
-        private void OnPlayerInput(BasePlayer player, InputState input)
-        {
-            if (player == null || input == null)
-            {
-                return;
-            }
-            
-            if (!(input.IsDown(BUTTON.FORWARD) 
-                  || input.IsDown(BUTTON.BACKWARD) 
-                  || input.IsDown(BUTTON.LEFT) 
-                  || input.IsDown(BUTTON.RIGHT)))
-            {
-                return;
-            }
-            
-            ComputerStation computerStation = player.GetMounted()?.GetComponentInParent<ComputerStation>();
-
-            if (computerStation == null)
-            {
-                return;
-            }
-
-            CCTV_RC cctvRc = computerStation.currentlyControllingEnt.Get(true).GetComponent<CCTV_RC>();
-                
             if (cctvRc == null || cctvRc.IsStatic())
             {
                 return;
             }
+            
+            cctvRc.hasPTZ = true;
+        }
 
-            float y = input.IsDown(BUTTON.FORWARD) ? 1f : (input.IsDown(BUTTON.BACKWARD) ? -1f : 0f);
-            float x = input.IsDown(BUTTON.LEFT) ? -1f : (input.IsDown(BUTTON.RIGHT) ? 1f : 0f);
+        private void OnEntityKill(ComputerStation computerStation)
+        {
+            BasePlayer player = computerStation.GetMounted();
 
-            InputState inputState = new InputState();
-            inputState.current.mouseDelta.y = y * _config.RotateSpeed;
-            inputState.current.mouseDelta.x = x * _config.RotateSpeed;
-            cctvRc.UserInput(inputState, player);
-
-            if (!_config.RotateSound)
+            if (player == null)
             {
                 return;
             }
             
-            EffectNetwork.Send(new Effect(MoveSound, cctvRc.transform.position, Vector3.zero));
+            UI.RemoveUI(player);
+        }
+        
+        private void OnEntityMounted(ComputerStation computerStation, BasePlayer player)
+        {
+            if (!HasPermission(player))
+            {
+                return;
+            }
+            
+            player.GetOrAddComponent<CameraMover>();
+        }
+
+        private void OnEntityDismounted(ComputerStation computerStation, BasePlayer player)
+        {
+            player.GetComponent<CameraMover>()?.Destroy();
+            
+            UI.RemoveUI(player);
+        }
+
+        private void OnBookmarkControl(ComputerStation computerStation, BasePlayer player, string bookmarkName, IRemoteControllable entity)
+        {
+            UI.RemoveUI(player);
+
+            CCTV_RC cctvRc = entity as CCTV_RC;
+
+            if (cctvRc == null || cctvRc.IsStatic())
+            {
+                return;
+            }
+            
+            if (!HasPermission(player))
+            {
+                return;
+            }
+
+            UI.CreateUI(player, Lang("Description", player.UserIDString));
+        }
+        
+        private void OnBookmarkControlEnd(ComputerStation station, BasePlayer player, CCTV_RC cctvRc)
+        {
+            UI.RemoveUI(player);
+        }
+        
+        #endregion
+
+        #region Core
+
+        private void CheckCCTV()
+        {
+            foreach(BaseNetworkable entity in BaseNetworkable.serverEntities)
+            {
+                CCTV_RC cctv = entity as CCTV_RC;
+                
+                if (cctv == null || cctv.IsStatic()) continue;
+                
+                cctv.hasPTZ = true;
+            }
+        }
+        
+        private class CameraMover : MonoBehaviour
+        {
+            private ComputerStation _computerStation;
+            private BasePlayer _player;
+
+            private void Awake()
+            {
+                _player = GetComponent<BasePlayer>();
+
+                if (_player == null)
+                {
+                    Destroy(this);
+                    return;
+                }
+
+                _computerStation = _player.GetMounted() as ComputerStation;
+            }
+
+            private void FixedUpdate()
+            {
+                CCTV_RC cctvRc = GetControlledCctv(_computerStation);
+
+                if (cctvRc == null || cctvRc.IsStatic())
+                {
+                    return;
+                }
+
+                float y = _player.serverInput.IsDown(BUTTON.FORWARD) ? 1f : (_player.serverInput.IsDown(BUTTON.BACKWARD) ? -1f : 0f);
+                float x = _player.serverInput.IsDown(BUTTON.LEFT) ? -1f : (_player.serverInput.IsDown(BUTTON.RIGHT) ? 1f : 0f);
+
+                InputState inputState = new InputState();
+                inputState.current.mouseDelta.y = y * _plugin._config.RotateSpeed;
+                inputState.current.mouseDelta.x = x * _plugin._config.RotateSpeed;
+
+                cctvRc.UserInput(inputState, _player);
+            }
+
+            public void Destroy() => Destroy(this);
+
+            private CCTV_RC GetControlledCctv(ComputerStation computerStation)
+            {
+                if (computerStation == null || computerStation.IsDestroyed)
+                {
+                    return null;
+                }
+                
+                return computerStation.currentlyControllingEnt.Get(serverside: true) as CCTV_RC;
+            }
         }
         
         #endregion
 
         #region UI
-        
-        private void CreateUI()
+
+        private static class UI
         {
-            _uiElements = new CuiElementContainer();
+            private const string PANEL_NAME = "MovableCCTV";
 
-            string panel = _uiElements.Add(new CuiPanel {
-                CursorEnabled = true,
-                Image = {
-                    Color = "0 0 0 0"
-                },
-                RectTransform = {
-                    AnchorMin = "0.293 0.903",
-                    AnchorMax = "0.684 0.951"
-                }
-            }, "Overlay", PanelName);
-
-            CuiLabel label = new CuiLabel
+            public static void CreateUI(BasePlayer player, string description = "")
             {
-                Text = {
-                    Text  = "",
-                    Align = TextAnchor.MiddleCenter,
-                    Color = "1 1 1 0.5",
-                    FontSize = 14
-                },
-                RectTransform = {
-                    AnchorMin = "0 0",
-                    AnchorMax = "1 1"
-                }
-            };
-
-            _textLabel = label.Text;
-
-            _uiElements.Add(label, panel);
-        }
-
-        private void UpdateUI(BasePlayer player)
-        {
-            _textLabel.Text = Lang("mounted", player.UserIDString);
+                CuiElementContainer container = new CuiElementContainer();
+                
+                string panel = container.Add(new CuiPanel {
+                    Image =
+                    {
+                        Color = "0 0 0 0"
+                    },
+                    RectTransform = {
+                        AnchorMin = _plugin._config.AnchorMin,
+                        AnchorMax = _plugin._config.AnchorMax
+                    }
+                }, "Overlay", PANEL_NAME);
+                
+                container.Add(new CuiLabel
+                {
+                    Text = {
+                        FontSize = _plugin._config.TextSize,
+                        Color = _plugin._config.TextColor,
+                        Text  = description,
+                        Align = TextAnchor.MiddleCenter
+                    },
+                    RectTransform = {
+                        AnchorMin = "0 0",
+                        AnchorMax = "1 1"
+                    }
+                }, panel);
+                
+                CuiHelper.AddUi(player, container);
+            }
             
-            CuiHelper.AddUi(player, _uiElements);
+            public static void RemoveUI(BasePlayer player) => CuiHelper.DestroyUi(player, PANEL_NAME);
+
+            public static void RemoveAll()
+            {
+                foreach (BasePlayer player in BasePlayer.activePlayerList)
+                {
+                    RemoveUI(player);
+                }
+            }
         }
-
-        private void DestroyUI(BasePlayer player) => CuiHelper.DestroyUi(player, PanelName);
-
+        
         #endregion
 
         #region Helpers
-        
+
         private string Lang(string key, string id = null, params object[] args) => string.Format(lang.GetMessage(key, this, id), args);
+        
+        private bool HasPermission(BasePlayer player) => permission.UserHasPermission(player.UserIDString, PERM_USE);
         
         #endregion
     }
